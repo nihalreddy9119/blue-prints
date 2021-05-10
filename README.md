e128d9174eb41df54e952e6c74ab49f1cf8532c0
nihalreddy9119 <74912738+nihalreddy9119@users.noreply.github.com>
2021-05-10 12:55:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# blue-prints
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,55 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, ball;
+var box1
+var box2
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box2=new Box(10,10,40,30)
+    box3=new Box(300,300,30,20)
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,400,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options ={
+        restitution: 1.0
+    }
+
+    ball = Bodies.circle(200,100,20, ball_options);
+    World.add(world,ball);
+
+
+
+    box=Bodies.rectangle(100,100,20,20,ball_options);
+    World.add(world,box);
+
+
+
+    console.log(ground);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
+ 
+    rectMode(CENTER);
+    rect(box.position.x,box.position.y,20,20);
+
+    box2.display();
+    box3.display();
+}
